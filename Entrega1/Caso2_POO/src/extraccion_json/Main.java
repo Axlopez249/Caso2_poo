2a122d8c15425c374fe176a3fab91a9ac3872671
Axel Lopez <axlopez@estudiantec.cr>
2023-10-05 14:56:01 -0600

    Contrato

--- Entrega1/Caso2_POO/src/extraccion_json/Main.java
+++ Entrega1/Caso2_POO/src/extraccion_json/Main.java
@@ -4,6 +4,24 @@ import java.util.ArrayList;
 import funcionamiento.*;
 
 public class Main {
+	
+	/*
+	 * Contrato 
+	 * Se le solicita al usuario que ingrese una región y una fecha válida: El programa empieza 
+	 * mostrando una UI que coleccione una fecha y una región. Se realizarán los movimientos necesarios 
+	 * para concretar formatos de fecha válidos
+	 * Por dentro del programa, se tomarán datos históricos de regiones y sus tiempos y climas provenientes 
+	 * de fuentes seguras. El programa toma datos o archivos json con la información y realiza una integración de los datos
+	 * Posteriormente, hace una búsqueda de la información solicitada con la que se tiene y se conecta con la UI del sistema
+	 * Finalmente, se mostrarán los datos a los usuarios por medio del traspaso de datos. En la pantalla se mostrará la información solicitada por el usuario respecto al pronostico de lluvia del día 
+	 * o informacion general del clima de ese día en específico con esa region
+	 * 
+	 * es un sistema de consulta únicamente, como decis, region y fecha, para hacer un pronóstico
+	 * dado que se ocupan muchos datos, podemos darle parámetros al json para que el programa auto 
+	 * genere los datos, salvo que tengas otra fuente para los datos
+	 *
+	 *Estrategia B del preliminar 2 
+	 */
 
 	public static void main(String[]args) {
 		Generador_Json g = new Generador_Json();
